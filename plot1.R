1163b84a7cd28ee87c443516ca21b800dd449a78
AmbroCzz <Ambrogio_Cazzaniga@hotmail.com>
2015-01-11 18:38:49 -0500

    Plot 1 exercise

---
+++ plot1.R
@@ -0,0 +1,7 @@
+# lOADING DATA
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, as.is = TRUE)
+# Selecting 2007-02-01 and 2007-02-02
+d1 <- data[ data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
+d1[ , "Global_active_power"] <- as.numeric(d1[ , "Global_active_power"])
+hist(x = d1$Global_active_power, col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
